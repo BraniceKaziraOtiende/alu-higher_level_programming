0ff38d1ba3a0390f947878d4888318afa547ede9
BraniceKaziraOtiende <branicekazira@gmail.com>
2024-02-23 08:37:44 -0800

    Add py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,5 +5,4 @@ a = 1
 b = 2
 
 if __name__ == '__main__':
-    print("{} + {} = {}".format(a, b, add(a, b)))#!/usr/bin/python3
-from add_0 import ad
+    print("{} + {} = {}".format(a, b, add(a, b)))
