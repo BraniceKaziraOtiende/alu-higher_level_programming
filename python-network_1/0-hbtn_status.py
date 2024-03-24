1827c885c00792e660806643613d73dfcf8bfd81
BraniceKaziraOtiende <branicekazira@gmail.com>
2024-03-24 12:27:29 -0700

    Status fix

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
 """
-import requests
 
+import urllib.request
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
